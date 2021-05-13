5b35a9d387b75e00f86c2c87f323da62c687d78a
Akshat Divya <akshatdivya@infrrd.ai>
2021-05-13 11:26:03 +0530

    add sort functionality in the home page

--- js/utility.js
+++ js/utility.js
@@ -4,10 +4,21 @@ function search() {
     console.log(herosHTMLCollection.childNodes)
     for (let i = 1; i < herosHTMLCollection.childNodes.length; i = i + 2) {
         if (!(herosHTMLCollection.childNodes[i].childNodes[3].innerHTML).match(key)) {
-            console.log((herosHTMLCollection.childNodes[i].childNodes[3].innerHTML));
             herosHTMLCollection.childNodes[i].style.display = "none"
         } else {
             herosHTMLCollection.childNodes[i].style.display = ""
         }
     }
+}
+
+function sortByName() {
+    let cardContainerIntems = document.getElementById("card-container");
+    let cardsCollection = cardContainerIntems.children;
+    let cards = [...cardsCollection];
+    cards.sort((card1, card2) => {
+        return card1.outerText > card2.outerText ? 1 : -1;
+    })
+    cards.forEach(card => {
+        cardContainerIntems.appendChild(card);
+    })
 }
