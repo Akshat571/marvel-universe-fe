659fcffd2e9bddc477e85cbe8bac0e53e1a728c9
Akshat Divya <akshatdivya@infrrd.ai>
2021-05-17 14:43:00 +0530

    fixes search and sort functions

--- js/utility.js
+++ js/utility.js
@@ -1,24 +1,38 @@
-function search() {
+function searchHome() {
     let key = (document.getElementById("search").value).toUpperCase();
     let herosHTMLCollection = document.getElementById("card-container");
-    console.log(herosHTMLCollection.childNodes)
-    for (let i = 1; i < herosHTMLCollection.childNodes.length; i = i + 2) {
-        if (!(herosHTMLCollection.childNodes[i].childNodes[3].innerHTML).match(key)) {
-            herosHTMLCollection.childNodes[i].style.display = "none"
+    let cardsCollection = herosHTMLCollection.children;
+    let cards = [...cardsCollection];
+    cards.forEach((card,cardIndex) => {
+        if(!(card.innerText).match(key)) {
+            herosHTMLCollection.children[cardIndex].style.display = "none";
         } else {
-            herosHTMLCollection.childNodes[i].style.display = ""
+            herosHTMLCollection.children[cardIndex].style.display = "";
+        }
+    })
+}
+
+function searchPopular() {
+    let key = (document.getElementById("search").value).toUpperCase();
+    let herosHTMLCollection = document.getElementById("card-container");
+    for (let i = 0; i < herosHTMLCollection.children.length; i++) {
+        if (!(herosHTMLCollection.children[i].children[2].innerHTML).match(key)) {
+            herosHTMLCollection.children[i].style.display = "none"
+        } else {
+            herosHTMLCollection.children[i].style.display = ""
         }
     }
 }
 
 function sortByName() {
-    let cardContainerIntems = document.getElementById("card-container");
-    let cardsCollection = cardContainerIntems.children;
+    let cardContainerItems = document.getElementById("card-container");
+    let cardsCollection = cardContainerItems.children;
     let cards = [...cardsCollection];
+    // console.log(cards);
     cards.sort((card1, card2) => {
-        return card1.outerText > card2.outerText ? 1 : -1;
+        return card1.innerText > card2.innerText ? 1 : -1;
     })
     cards.forEach(card => {
-        cardContainerIntems.appendChild(card);
+        cardContainerItems.appendChild(card);
     })
 }
